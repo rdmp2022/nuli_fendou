d0e7b3e107aed289a5ee7f49595a39f80ab1f0ed
yy <wmyaoo@outlook.com>
2022-09-18 00:56:21 +0800

    first commit

---
+++ src/com/walker/thread/threadTest.java
@@ -0,0 +1,18 @@
+package com.walker.thread;
+
+
+/**
+ * @author Walker
+ * @date 2022/9/15
+ */
+public class threadTest {
+
+}
+
+/**
+ * 创建继承于Thread类的线程子类
+ * @author Walker
+ */
+class ThreadDemo extends Thread{
+
+}
